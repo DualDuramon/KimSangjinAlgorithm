ffb1ff85196043c155fd467f6304cafbbc2c76bf
DualDura <jdsan216@gmail.com>
2024-04-11 23:28:50 +0900

    Base_Commit

---
+++ SangjinAlgorithm/Exercise/Exercise.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+/*
+* 알고리즘 및 실습 02분반
+* 2019136003 강윤민
+* 2024년도 1학기 알고리즘및실습 6장
+* 문제 A : 최댓값 최솟값 동시에 찾기
+* 
+* 
+*/
+
+
+void TestCase() {
+    int n = 0;
+    std::cin >> n;
+
+    std::vector<int> vec(n);
+
+    for (int i = 0; i < n; i++) {
+        std::cin >> vec[i];
+    }
+
+
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int t{ 0 };
+    std::cin >> t;
+    for (int i = 0; i < t; i++)
+        TestCase();
+
+    return 0;
+}
