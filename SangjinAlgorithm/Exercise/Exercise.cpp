939246f0903a63e5a889cefea0e2a33016dc1fd9
DualDura <jdsan216@gmail.com>
2024-04-12 16:41:38 +0900

    B is Solved

--- SangjinAlgorithm/Exercise/Exercise.cpp
+++ SangjinAlgorithm/Exercise/Exercise.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
-#include<algorithm>
 #include<vector>
+#include<climits>
 
 using namespace std;
 /*
@@ -11,12 +11,12 @@ using namespace std;
 * 토너먼트 배열(이진 힙 구조, 트리구조)을 거꾸로 올라가며 만들어가는 형식
 * 반대자 전략 - 토너먼트 구조를 이용 해서 2등을 찾는 것.
 * 자식노드들을 비교해 부모노드의 값을 정하면서 루트 노드까지 올라감 -> 인덱스는 2단위로 감소
-* 루트노드(1등)이 정해지면 루트노드의 자식들을 비교해 루트노드 != 자식노드 인 값을 반환함.
+* 토너먼트를 진행하면서 2등의 값도 같이 저장하면서 진행.
 */
 
 int FindSecondMax(std::vector<int>& vec) {
 
-    std::vector<int> tourVec(2 * vec.size() - 1);   //토너먼트 배열
+    std::vector<int> tourVec(2 * vec.size() - 1, 0);   //토너먼트 배열
 
     for (int i = vec.size() - 1; i >= 0; i--) { //토너먼트 배열 끝에 vec 붙이기
         tourVec[vec.size() - 1 + i] = vec[i];
@@ -26,15 +26,20 @@ int FindSecondMax(std::vector<int>& vec) {
     int secondMax = INT_MIN;
    
     while (0 < idx) {
-        tourVec[(idx - 2) / 2] = tourVec[idx - 1] < tourVec[idx] ? tourVec[idx] : tourVec[idx - 1];     //i번 인덱스의 자식 노드 : 2i+1, 2i+2
-        
+        tourVec[(idx - 2) / 2] = std::max(tourVec[idx - 1], tourVec[idx]);
+        if (tourVec[idx - 1] != tourVec[idx]) {//서로 비교하는 두 수가 같지 않을 경우에만 2등 찾기를 진행.
+            if (tourVec[idx - 1] < tourVec[idx]) {
+                secondMax = secondMax < tourVec[idx - 1] ? tourVec[idx - 1] : secondMax;
+            }
+            else {
+                secondMax = secondMax < tourVec[idx] ? tourVec[idx] : secondMax;
+            }
+        }
         idx -= 2;
     }
 
-
-    
-
-    return tourVec[0];
+    //2등이 정해지지 않으면 secondMax는 INT_MIN을 가지고 있게됨. 고로 1등 반환
+    return secondMax == INT_MIN ? tourVec[0] : secondMax;
 }
 
 void TestCase() {
