340843629aa2355ec2fdc6dd22db5fab814fb3e6
DualDura <jdsan216@gmail.com>
2024-04-13 01:32:01 +0900

    C Clear

--- SangjinAlgorithm/Exercise/Exercise.cpp
+++ SangjinAlgorithm/Exercise/Exercise.cpp
@@ -1,45 +1,35 @@
 #include<iostream>
 #include<vector>
-#include<climits>
+#include<algorithm>
 
 using namespace std;
 /*
 * 알고리즘 및 실습 02분반
 * 2019136003 강윤민
 * 2024년도 1학기 알고리즘및실습 6장
-* 문제 B : 두번째로 큰 값 찾기
-* 토너먼트 배열(이진 힙 구조, 트리구조)을 거꾸로 올라가며 만들어가는 형식
-* 반대자 전략 - 토너먼트 구조를 이용 해서 2등을 찾는 것.
-* 자식노드들을 비교해 부모노드의 값을 정하면서 루트 노드까지 올라감 -> 인덱스는 2단위로 감소
-* 토너먼트를 진행하면서 2등의 값도 같이 저장하면서 진행.
+* 문제 C : wiggleSort
+* 배열을 오름차 순으로 정렬한 후 중앙값보다 높은 부분을 high 부분, 낮은 부분을 low부분이라 하면
+* low에서 하나, high에서 하나 선택해서 차례대로 출력함. 하지만 중앙값쪽에 중복된 숫자가 몰려있으면
+* low에서 중복된 수를 뽑을땐 최대한 빠르게 뽑고, high에서 중복된 수를 뽑을 땐 최대한 늦게 뽑게 하기 위해
+* 둘 다 각 영역의 끝에서부터 수 뽑기를 진행한다. 중복된 수가 low영역에선 끝부분에, high영역에선 처음 부분에
+* 분포되어 있기 때문이다.
 */
 
-int FindSecondMax(std::vector<int>& vec) {
+void WiggleSort(std::vector<int>& vec) {
 
-    std::vector<int> tourVec(2 * vec.size() - 1, 0);   //토너먼트 배열
+    std::vector<int> wiggle(vec);   //wiggle 만드는 배열
 
-    for (int i = vec.size() - 1; i >= 0; i--) { //토너먼트 배열 끝에 vec 붙이기
-        tourVec[vec.size() - 1 + i] = vec[i];
+    int left = (vec.size() - 1)/ 2; //기존 정렬된 배열의 중앙부터 시작
+    int right = vec.size() - 1;     //기존 정렬된 배열의 끝부터 시작
+
+    for (int i = 0; i < wiggle.size(); i++) {
+        wiggle[i] = i % 2 == 0 ? vec[left--] : vec[right--];
     }
 
-    int idx = tourVec.size() - 1;
-    int secondMax = INT_MIN;
-   
-    while (0 < idx) {
-        tourVec[(idx - 2) / 2] = std::max(tourVec[idx - 1], tourVec[idx]);
-        if (tourVec[idx - 1] != tourVec[idx]) {//서로 비교하는 두 수가 같지 않을 경우에만 2등 찾기를 진행.
-            if (tourVec[idx - 1] < tourVec[idx]) {
-                secondMax = secondMax < tourVec[idx - 1] ? tourVec[idx - 1] : secondMax;
-            }
-            else {
-                secondMax = secondMax < tourVec[idx] ? tourVec[idx] : secondMax;
-            }
-        }
-        idx -= 2;
-    }
-
-    //2등이 정해지지 않으면 secondMax는 INT_MIN을 가지고 있게됨. 고로 1등 반환
-    return secondMax == INT_MIN ? tourVec[0] : secondMax;
+    std::cout << wiggle.size() << "\n";     //출력
+    for (int i : wiggle)
+        std::cout << i << " ";
+    std::cout << "\n";
 }
 
 void TestCase() {
@@ -51,8 +41,8 @@ void TestCase() {
     for (int i = 0; i < n; i++) {
         std::cin >> vec[i];
     }
-    std::cout << FindSecondMax(vec) << "\n";
-
+    std::sort(vec.begin(), vec.end());
+    WiggleSort(vec);
 }
 
 int main(void) {
