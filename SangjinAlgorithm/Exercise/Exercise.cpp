d08c891d68a60b6018585f7b5d9961a1d460b20a
DualDura <jdsan216@gmail.com>
2024-04-16 00:01:30 +0900

    Chap7_A Clear

--- SangjinAlgorithm/Exercise/Exercise.cpp
+++ SangjinAlgorithm/Exercise/Exercise.cpp
@@ -1,48 +1,58 @@
 #include<iostream>
-#include<vector>
 #include<algorithm>
+#include<vector>
+
 
 using namespace std;
 /*
 * 알고리즘 및 실습 02분반
 * 2019136003 강윤민
-* 2024년도 1학기 알고리즘및실습 6장
-* 문제 C : wiggleSort
-* 배열을 오름차 순으로 정렬한 후 중앙값보다 높은 부분을 high 부분, 낮은 부분을 low부분이라 하면
-* low에서 하나, high에서 하나 선택해서 차례대로 출력함. 하지만 중앙값쪽에 중복된 숫자가 몰려있으면
-* low에서 중복된 수를 뽑을땐 최대한 빠르게 뽑고, high에서 중복된 수를 뽑을 땐 최대한 늦게 뽑게 하기 위해
-* 둘 다 각 영역의 끝에서부터 수 뽑기를 진행한다. 중복된 수가 low영역에선 끝부분에, high영역에선 처음 부분에
-* 분포되어 있기 때문이다.
+* 2024년도 1학기 알고리즘및실습 7장
+* 문제 A : FindCycle
+* 
 */
 
-void WiggleSort(std::vector<int>& vec) {
-
-    std::vector<int> wiggle(vec);   //wiggle 만드는 배열
-
-    int left = (vec.size() - 1)/ 2; //기존 정렬된 배열의 중앙부터 시작
-    int right = vec.size() - 1;     //기존 정렬된 배열의 끝부터 시작
-
-    for (int i = 0; i < wiggle.size(); i++) {
-        wiggle[i] = i % 2 == 0 ? vec[left--] : vec[right--];
+bool isCycleDFS(std::vector<std::vector<int>>& graph, int startNode, int thisNode, std::vector<bool>& visited) {
+    visited[thisNode] = true;
+    
+    for (int i = 0; i < graph[thisNode].size(); i++) {
+        int nextNode = graph[thisNode][i];
+        if (startNode == nextNode) return true;
+        
+        if (!visited[nextNode]&& isCycleDFS(graph, startNode, nextNode, visited)) {
+            return true;
+        }
     }
+    return false;
+}
 
-    std::cout << wiggle.size() << "\n";     //출력
-    for (int i : wiggle)
-        std::cout << i << " ";
-    std::cout << "\n";
+
+void FindCycle(std::vector<std::vector<int>>& graph) {
+    
+    for (int i = 0; i < graph.size(); i++) {
+        std::vector<bool>visited(graph.size(), false);
+        if (isCycleDFS(graph, i, i, visited)) {
+            std::cout << "true\n";
+            return;
+        }
+    }
+    std::cout << "false\n";
 }
 
 void TestCase() {
-    int n = 0;
-    std::cin >> n;
+    int n = 0, e = 0;
+    std::cin >> n >> e;
 
-    std::vector<int> vec(n);
+    std::vector<std::vector<int>> graph(n, std::vector<int>());
 
-    for (int i = 0; i < n; i++) {
-        std::cin >> vec[i];
+    for (int i = 0; i < e; i++) {
+        int node1 = 0, node2 = 0;
+        std::cin >> node1 >> node2;
+        graph[node1].push_back(node2);
     }
-    std::sort(vec.begin(), vec.end());
-    WiggleSort(vec);
+
+    FindCycle(graph);
+
 }
 
 int main(void) {
