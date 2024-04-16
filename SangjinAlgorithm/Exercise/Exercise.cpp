06182f8bff9e0e0ac422a0d36f584ec62dafab81
DualDura <jdsan216@gmail.com>
2024-04-17 02:30:29 +0900

    Solving C

--- SangjinAlgorithm/Exercise/Exercise.cpp
+++ SangjinAlgorithm/Exercise/Exercise.cpp
@@ -9,63 +9,89 @@ using namespace std;
 * 알고리즘 및 실습 02분반
 * 2019136003 강윤민
 * 2024년도 1학기 알고리즘및실습 7장
-* 문제 B : 최단거리 찾기
-* 우선순위 큐를 이용한 다익스트라 구현
+* 문제 C : 최적의 변환 순서 찾기
+* BFS를 사용하는 문제
 */
 
-typedef std::pair<int, int> edge;   //pair<가중치, 도착노드>
+void findShortcutWord(std::vector<std::vector<bool>>& graph, std::vector<string>& dictionary, int startIdx, string word, string destWord) {
 
-void Dijkstra(std::vector<std::vector<edge>>& graph, std::vector<int>& destVec, int startNode){
+    std::vector<bool> visited(dictionary.size(), false);
+    std::vector<int> dist(dictionary.size(), INT_MAX);
+    std::queue<int> queue;
+    queue.push(startIdx);
+    dist[startIdx] = 0;
 
-    std::vector<int> distance(graph.size(), INT_MAX);
-    std::priority_queue<int> dijkQ;
-    std::vector<bool> visited(graph.size(), false);
-  
-    dijkQ.push(startNode);
-    distance[startNode] = 0;
-
-    while (!dijkQ.empty()) {
-        int nowNode = dijkQ.top();
+    while (!queue.empty()) {
+        int nowNode = queue.front();
         visited[nowNode] = true;
-        dijkQ.pop();
+        queue.pop();
 
-        for (edge e : graph[nowNode]) {
-            int nextNode = e.second;
-            int nextCost = e.first;
-
-            if (distance[nextNode] > distance[nowNode] + nextCost) {
-                distance[nextNode] = distance[nowNode] + nextCost;
-                dijkQ.push(e.second);
+        for (int i = 0; i < graph[nowNode].size(); i++) {
+            if (graph[nowNode][i] && !visited[i]) {
+                if (dist[i] > dist[nowNode] + 1) {
+                    dist[i] = dist[nowNode] + 1;
+                    visited[i] = false;
+                }
+                queue.push(i);
             }
         }
     }
 
-    for (int i = 0; i < destVec.size(); i++) {
-        std::cout << (distance[destVec[i]] == INT_MAX ? -1 : distance[destVec[i]]) << " ";
-    }
-    std::cout << "\n";
 }
 
+int compareTwoWord(string str1, string str2) {
+    int counter = 0;
+    for (int idx = 0; idx < str1.size(); idx++) {
+        if (str1[idx] != str2[idx]) counter++;
+    }
+
+    return counter;
+}
 
 void TestCase() {
-    int n = 0, e = 0, startNode = 0, arrNodeNum = 0;
-    std::cin >> n >> e >> startNode >> arrNodeNum;
+    bool notInclude = true;
+    int dictSize = 0;
+    string word = "", destWord = "";
+    std::cin >> word >> destWord >> dictSize;
 
-    std::vector<std::vector<edge>> graph(n, std::vector<edge>());   //가중치 그래프
-    std::vector<int> destVec(arrNodeNum);       //목적지노드배열
+    std::vector<string>dictionary(dictSize + 1);
+    dictionary[0] = word;
 
-    for (int i = 0; i < arrNodeNum; i++) {
-        std::cin >> destVec[i];
+    //단어배열 생성
+    for (int i = 1; i < dictionary.size(); i++) {
+        std::cin >> dictionary[i];
+        if (notInclude && dictionary[i] == destWord) 
+            notInclude = false;
     }
 
-    for (int i = 0; i < e; i++) {
-        int node1, node2, cost;
-        std::cin >> node1 >> node2 >> cost;
-
-        graph[node1].push_back({ cost,node2 });
+    if (notInclude) { //끝단어가 사전에 없을 경우
+        std::cout << -1 << "\n";
+        return;
     }
 
-    Dijkstra(graph, destVec, startNode);
+    //시작위치 지정
+    int startIdx = 0;
+    for (int i = 0; i < dictionary.size(); i++) {
+        int count = compareTwoWord(destWord, dictionary[i]);
+        if (count == 0 || 1) {
+            startIdx = i;
+            break;
+        }
+    }
+
+    //그래프 초기화
+    std::vector<std::vector<bool>>graph(dictSize + 1, std::vector<bool>(dictSize + 1, false));
+
+    for (int i = 0; i < graph.size(); i++) {
+        for (int j = i; j < graph[i].size(); j++) {
+            if (compareTwoWord(dictionary[i], dictionary[j]) == 1) {
+                graph[i][j] = graph[j][i] = true;
+            }
+        }
+    }
+
+
+    //findShortcutWord(graph, dictionary, startIdx, word, destWord);
 }
 
 int main(void) {
