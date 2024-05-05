3d3fc0309a56a992689b0645f2473c9cc97100d6
DualDura <jdsan216@gmail.com>
2024-05-06 02:22:30 +0900

    solving B

--- SangjinAlgorithm/Exercise/Exercise.cpp
+++ SangjinAlgorithm/Exercise/Exercise.cpp
@@ -1,76 +1,42 @@
 #include<iostream>
 #include<vector>
+#include<string>
 #include<algorithm>
 
+
 using namespace std;
+
 /*
 * 알고리즘 및 실습 02분반
 * 2019136003 강윤민
 * 2024년도 1학기 알고리즘및실습 8장
-* 문제 A : 마감시간이 있는 최적의 스케쥴 짜기
-* 
+* 문제 B : 가장 큰 수 만들기
 * 
 */
-
-//typedef std::pair<int, int> work;   //work = pair<이득, 종료시간>
-
-struct work {
-    int id = 0;
-    int benefit = 0;
-    int endTime = 0;
-
-    bool operator<(const work& w) {
-        if (endTime == w.endTime)
-            return id < w.id;
-        else
-            return endTime < w.endTime;
-    }
-};
-
-bool isPossible(std::vector<work>& k) {
-    std::sort(k.begin(), k.end());
-
-    for (int idx = 0; idx < k.size() - 1; idx++) {
-        if (k[idx].endTime >= k[idx + 1].endTime) return false;
+std::string MaxNumber(std::vector<string>& nums) {
+    string ans = "";
+    
+    for (auto str : nums) {
+        ans += str;
     }
 
-    return true;
+    return ans;
 }
 
 
-void scheduling(std::vector<work>& vec) {
-    std::vector<work> schedule;
-
-    schedule.push_back(vec[0]);
-
-    for (int i = 1; i < vec.size(); i++) {
-        std::vector<work> k = schedule;
-        k.push_back(vec[i]);
-        if (isPossible(k)) schedule = k;
-    }
-
-    for (auto& k : schedule)
-        std::cout << k.id << " ";
-    std::cout << "\n";
-
-}
 
 void TestCase() {
-    int n{ 0 };
+    int n = 0;
     std::cin >> n;
 
-    std::vector<work>vec(n);
+    std::vector<string> nums(n);
 
-    for (int i = 0; i < n; i++) {
-        vec[i].id = i + 1;
-        std::cin >> vec[i].endTime >> vec[i].benefit;
-    }
+    for (int i = 0; i < n; i++) 
+        std::cin >> nums[i];
 
-    std::sort(vec.begin(), vec.end(), [](work a, work b) { return a.benefit > b.benefit; });
+    std::sort(nums.begin(), nums.end());
 
-
-    scheduling(vec);
-
+    //std::cout << MaxNumber(nums) << "\n";
 }
 
 int main(void) {
