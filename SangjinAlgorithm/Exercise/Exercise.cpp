c3a6cc526af4d3dbb46d8ebc7375aac513380f34
DualDura <jdsan216@gmail.com>
2024-05-09 13:40:17 +0900

    s

--- SangjinAlgorithm/Exercise/Exercise.cpp
+++ SangjinAlgorithm/Exercise/Exercise.cpp
@@ -12,16 +12,53 @@ using namespace std;
 * 우선순위 큐를 이용한 Prim 알고리즘 이용
 *
 */
-typedef std::pair<int, int> edge; //tuple<도착노드 , 가중치>
+struct edge {
+    int startNode = 0;
+    int endNode = 0;
+    int weight = 0;
+};
+
+struct cmp {
+    bool operator() (edge& e1, edge& e2) {
+        return e1.weight > e2.weight;
+    }
+};
 
 int prim(std::vector<std::vector<edge>> graph, int startNode, int nodeSize) {
     
     std::vector<bool> visited(nodeSize, false);
     std::vector<int> score(nodeSize, 100);  //100 = INT_MAX
 
+    std::priority_queue < edge, std::vector<edge>, cmp> pq;
 
+    for (auto e : graph[startNode]) {
+        score[e.endNode] = e.weight;
+        pq.push(e);
+    }
 
-    return 0;
+    while (!pq.empty()) {
+        auto [nowNode, nextNode, w] = pq.top();
+        pq.pop();
+
+        visited[nextNode] = true;
+
+        for (edge nextEdge : graph[nextNode]) {
+            if (!visited[nextEdge.endNode]) {
+                if (nextEdge.weight < score[nextEdge.endNode]) {
+                    score[nextEdge.endNode] = nextEdge.weight;
+                    pq.push(edge{ nextEdge.startNode,nextEdge.endNode,score[nextEdge.endNode] });
+                }
+            }
+        }
+    }
+    
+    int ans = 0;
+    for (auto i : score) {
+        if (i == 100) continue;
+        ans += i;
+    }
+
+    return ans;
 }
 
 void TestCase() {
@@ -33,11 +70,12 @@ void TestCase() {
     for (int i = 0; i < e; i++) {
         int s, e, w;
         std::cin >> s >> e >> w;
-        graph[s].push_back(std::make_pair(e, w));
+        graph[s].push_back(edge{ s,e,w });
+
     }
 
     int answer = prim(graph, 0, n);
-    
+    std::cout << answer << "\n";
 }
 
 int main(void) {
