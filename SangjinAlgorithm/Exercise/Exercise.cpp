7eddbca978034c7f1991dddef10c9aeb1e929fa1
DualDura <jdsan216@gmail.com>
2024-05-14 22:35:20 +0900

    solve C

--- SangjinAlgorithm/Exercise/Exercise.cpp
+++ SangjinAlgorithm/Exercise/Exercise.cpp
@@ -1,82 +1,53 @@
 #include<iostream>
 #include<vector>
-#include<queue>
 #include<algorithm>
 
 using namespace std;
 /*
 * 알고리즘 및 실습 02분반
 * 2019136003 강윤민
-* 2024년도 1학기 알고리즘및실습 9장
-* 문제 A : 가중치 무방향 그래프 최소신장트리 구하기
-* 우선순위 큐를 이용한 Prim 알고리즘 이용
-*
+* 2024년도 1학기 알고리즘및실습 10장
+* 문제 C : 합이 같은 분할 찾기
+* 테뷸레이션으로 풀어보기
+*   - 주어진 수를 전부 합한 값의 절반이 되는지를 검사하면 됨.
+*   - 합이 홀수이면 절반이 되는 값을 만들 수 없음.
 */
-struct edge {
-    int node1 = 0;
-    int node2 = 0;
-    int weight = 0;
-};
 
-struct cmp {
-    bool operator() (edge& e1, edge& e2) {
-        return e1.weight > e2.weight;
-    }
-};
 
-int prim(std::vector<std::vector<edge>> graph, int startNode, int nodeSize) {
-    std::vector<bool> visited(nodeSize, false);
-    visited[startNode] = true;
-    std::vector<int> score(nodeSize, 100);    //100 = INTMAX
-    std::vector<edge> tree;
+bool FindSub(std::vector<int>& vec, int targetNum) {
+    std::vector<bool> table(targetNum + 1, false);
+    table[0] = true;
 
-    std::priority_queue<edge, std::vector<edge>, cmp> pq;
-
-    for (edge& e : graph[startNode]) {
-        score[e.node2] = e.weight;
-        pq.push(e);
-    }
-
-    while (!pq.empty()) {
-        edge nowEdge = pq.top();
-        pq.pop();
-
-        if (!visited[nowEdge.node2])tree.push_back(nowEdge);
-        visited[nowEdge.node2] = true;
-
-        for (auto& nextEdge : graph[nowEdge.node2]) {
-            if (!visited[nextEdge.node2]) {
-                if (nextEdge.weight < score[nextEdge.node2]) {
-                    score[nextEdge.node2] = nextEdge.weight;
-                    pq.push(nextEdge);
-                }
+    for (int i = 0; i < targetNum; i++) {
+        if (table[i]) {
+            for (int x : vec) {
+                if (i + x <= targetNum) table[i + x] = true;
             }
         }
-
     }
 
-    int ans = 0;
-    for (auto& e : tree)
-        ans += e.weight;
-
-    return ans;
+    return table[targetNum];
 }
 
+
 void TestCase() {
-    int n{ 0 }, e{ 0 };
-    std::cin >> n >> e;
+    int n{ 0 }, m{ 0 };
+    std::cin >> n;
 
-    std::vector<std::vector<edge>>graph(n, std::vector<edge>());
 
-    for (int i = 0; i < e; i++) {
-        int s, e, w;
-        std::cin >> s >> e >> w;
-        graph[s].push_back(edge{ s,e,w });
-        graph[e].push_back(edge{ e,s,w });
+    std::vector<int> vec(n, 0);
+
+    for (int i = 0; i < n; i++) {
+        std::cin >> vec[i];
+        m += vec[i];
     }
 
-    int answer = prim(graph, 0, n);
-    std::cout << answer << "\n";
+    if (m % 2 == 1) {
+        std::cout << "false\n";
+        return;
+    }
+
+    std::cout << std::boolalpha << FindSub(vec, m / 2) << "\n";
 }
 
 int main(void) {
